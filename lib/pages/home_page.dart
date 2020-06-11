3fcbd8363da7913cd208c1f724e47f6cbd0a037d
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2020-06-11 17:16:06 -0300

    Adiciona NavigationBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,31 +7,56 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+    ),
+    Text(
+      'Index 1: Business',
+    ),
+    Text(
+      'Index 2: School',
+    ),
+  ];
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              _buildTitle('Futuro Menu'),
-              _buildTitle('Dinheiro Guardado'),      
-              _buildTitle('Gastos do Mês'),
-              _buildTitle('...'),
-            ],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Business'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            title: Text('School'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
     );
   }
 
   _buildAppBar(){
     return AppBar(
-      title: Text('Economize',
+      title: Text('PA',
         style: TextStyle(
           color: Colors.white,
           fontWeight: FontWeight.w900,
@@ -51,9 +76,9 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  _buildTitle(String title, {double top = 16.0, double bottom = 8.0}){
+  _buildText(String title, {double top = 16.0, double bottom = 8.0}){
     return Container(
-      padding: EdgeInsets.fromLTRB(0.0, top, 0.0, bottom),
+      padding: EdgeInsets.only(left: 5, right: 5, top: 5, bottom: 5),
       child: Text(
         title,
         style: TextStyle(
@@ -63,4 +88,5 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
 }
