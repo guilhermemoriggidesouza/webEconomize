69ec6cfe2110acb37dc4c477a3d33a63c03aad79
Andreeeez <57879149+Andreeeez@users.noreply.github.com>
2020-07-02 21:35:52 -0300

    Tela Meta P1

--- lib/view/metas.dart
+++ lib/view/metas.dart
@@ -7,70 +7,126 @@ class Metas extends StatefulWidget {
 }
 
 class _MetasState extends State<Metas> {
-  final List<String> mensagem = ["1 diuwecwlcnlrwkfwefewfefewfewffw", "2 eewddewdeedwedewdedweded", "3 ewdewdewdewdewdewdedewdwd", "4 wfwfewfqefefwefwfwefwef"];
+  final List<String> mensagem = [
+    "é simplesmente um texto fictício da indústria tipográfica e de impressão." +
+    "Lorem Ipsum é o texto fictício padrão do setor desde os anos 1500",
+    "é simplesmente um texto fictício da indústria tipográfica e de impressão." +
+    "Lorem Ipsum é o texto fictício padrão do setor desde os anos 1500",
+    "é simplesmente um texto fictício da indústria tipográfica e de impressão." +
+    "Lorem Ipsum é o texto fictício padrão do setor desde os anos 1500",
+    "é simplesmente um texto fictício da indústria tipográfica e de impressão." +
+    "Lorem Ipsum é o texto fictício padrão do setor desde os anos 1500",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       backgroundColor: Color(0xff1B384A),
-      body: 
-        Container(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
-        height: MediaQuery.of(context).size.height * 0.35,
-        child: Column(
-         children: <Widget>[
-          Text("Metas não concluidas"),
-          ListView.builder(
-            scrollDirection: Axis.horizontal,
-              itemCount: mensagem.length, itemBuilder: (context, index) {
-                return Container(
-                  width: MediaQuery.of(context).size.width * 0.6,
-                  child: Card(
-                    color: Color(0xff142129),
-                    child: Column(
-                      children: <Widget>[
-                        Text(mensagem[index], style: TextStyle(color: Colors.white, fontSize: 10.0),),
-                        new FlatButton(
-                          child: const Text('Confirmar'),
-                          color: Colors.blue,
-                          onPressed: () { /* ... */ },
-                        ),
-                        new FlatButton(
-                          child: const Text('Excluir'),
-                          color: Colors.red,
-                          onPressed: () { /* ... */ },
-                        )
-                      ]
-                    )
-                  ),
-          
-                );
-              }
-            ),
-          Text("Metas concluidas"),
-          ListView.builder(
-            scrollDirection: Axis.horizontal,
-              itemCount: mensagem.length, itemBuilder: (context, index) {
-                return Container(
-                  width: MediaQuery.of(context).size.width * 0.6,
-                  child: Card(
-                    color: Color(0xff142129),
-                    child: Column(
-                      children: <Widget>[
-                        Text(mensagem[index], style: TextStyle(color: Colors.white, fontSize: 10.0),),
-                        new FlatButton(
-                          child: const Text('Excluir'),
-                          color: Colors.red,
-                          onPressed: () { /* ... */ },
-                        ),
-                      ]
-                    )
-                  ),
-                );
-              }
-            ),
-         ]
-        )
-      )
+      body: _buildBody(),
     );
+  }
+
+  _buildBody() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(top: 15.0),
+          child: Container(
+            width: double.infinity,
+            child: Text(
+              "Metas não concluida",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white, 
+                fontSize: 22,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+          height: MediaQuery.of(context).size.height * 0.35,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: mensagem.length,
+            itemBuilder: (context, index) {
+            return Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              child: Card(
+                color: Colors.black.withOpacity(0.5),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0, bottom: 5.0, left: 5.0, right: 5.0),
+                      child: Container(
+                        child: Text(
+                          "Titulo movimento da saida",
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 20.0, left: 40, right: 40, bottom: 45),
+                        child: Text(
+                          mensagem[index],
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 13
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container( 
+                      child: Padding(
+                        padding: EdgeInsets.all(1),
+                        child: Row(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(right: 6.0),
+                              child: Container(
+                                width: 115,
+                                height: 45,
+                                child: RaisedButton(
+                                  elevation: 0,
+                                  color: Colors.green,
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Confirmar",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 45,
+                              width: 115,
+                              child: RaisedButton(
+                                elevation: 0,
+                                color: Colors.red,
+                                onPressed: () {},
+                                child: Text(
+                                  "Excluir",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ), 
+              ),
+            );
+          }),
+        ),
+      ],
+    );
+  }
+
+  _buildList(){
+
   }
 }
