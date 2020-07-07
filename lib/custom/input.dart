5fc463fe34a0131ec689e75f39e7ba2d93bbcf49
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2020-07-06 22:32:00 -0300

    Altera input

--- lib/custom/input.dart
+++ lib/custom/input.dart
@@ -19,16 +19,39 @@ class InputLabel extends StatelessWidget {
       height: 55,
       padding: EdgeInsets.symmetric(vertical: 8.0),
       child: TextFormField(
+        style: TextStyle(color: Colors.white, decorationColor: Colors.white),
         obscureText: obscureInput == null ? false : obscureInput,
         keyboardType: TextInputType.text,
         onSaved: onSaved,
         maxLines: 1,
         decoration: InputDecoration(
-          fillColor: Colors.white,
-          labelText: label,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(3),
+          filled: true,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1,color: Colors.white),
           ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1,color: Colors.white),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1,color: Colors.white),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1,)
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1,color: Colors.white)
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            borderSide: BorderSide(width: 1,color: Colors.white)
+          ),
+          hintText: label,
+          hintStyle: TextStyle(fontSize: 18,color: Color(0xFFB3B1B1))
         ),
       ),
     );
